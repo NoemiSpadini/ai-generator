1b100921d28a35f057851618715cdd9052f46dcb
NoemiSpadini <noemispadini94@gmail.com>
2023-12-12 14:58:35 +0100

    fixed the AI searching

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 function displayPoem(response) {
-  console.log("poem generated");
-  new Typewriter("poem generated", {
+  new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
     delay: 1,
@@ -14,19 +13,15 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "f470bb0040dca6ffoa47t3c9d8dcf220";
   let context =
-    "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem page and NOT at the beginning";
-  let prompt = `User instructions: Genrate Italian poem about ${instructionsInput.value}`;
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+  let prompt = `User instructions: Generate an Italian poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  //Build the API URL
-  //Make a call to the API
-
-  console.log("Generating poem");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating an Italian poem about ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayPoem);
-  //Display generated poems
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
